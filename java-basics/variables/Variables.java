3ffec9ae5f57f35a7bae69bd88b431531edf1761
ajit patil <ajitpatil.10@outlook.com>
2023-02-20 12:50:22 +0530

    Started Java and Springboot basics

---
+++ java-basics/variables/Variables.java
@@ -0,0 +1,12 @@
+/**
+ * Variables
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+        int busTicket = passengers;
+        System.out.println(busTicket);
+    }
+}
